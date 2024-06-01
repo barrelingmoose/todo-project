a22dacf8884ab44302c357da42a35ba59716dcee
Chandler Kramer <ck62013@gmail.com>
2024-05-31 21:54:36 -0500

    Removing gui from this branch

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,10 @@ mod monitor;
 mod data; 
 mod operations; 
 mod json_helper;
-mod gui_interface;
 
 
 pub use crate::monitor::Progress;
 pub use crate::data::TodoItem;
-pub use crate::gui_interface::TestUI;
-pub use egui::Ui;
-pub use eframe::egui;
-
-
 fn main() {
-    let app: TestUI = TestUI;
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native("Todo App", native_options, Box::new(|_cc| Box::new(app)));
     operations::run(); 
 }
